36ad2a6701c683151b77a603553eb4dfdb6ce0ff
khoanx1007 <khoanx1007@gmail.com>
2023-07-20 17:22:52 +0700

    chapter 3 up

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,5 +9,8 @@ class Task extends Model
 {
     use HasFactory;
     protected  $table = 'tasks';
-
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
